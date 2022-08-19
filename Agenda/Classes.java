60c2ef5e3775c9a7992e6c5f2284d1daea41ddff
Da niel <95182927+danievmg@users.noreply.github.com>
2022-08-19 11:06:39 -0300

    Agenda
    Agenda Incompleta

---
+++ Agenda/Classes.java
@@ -0,0 +1,52 @@
+package Agenda;
+
+public class Classes {
+    private int codigo;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    // Metodos de .
+
+    // Metodos Construtores
+
+    public Classes(int codigo, String nome, String email, String telefone) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
